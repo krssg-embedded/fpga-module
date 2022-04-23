52b858d7058c69c91811c13ba1b6be2f90673311
bluecoffee093 <agrawalyash093@gmail.com>
2022-04-24 01:08:47 +0530

    corrected commutation in gatedriver

--- gatedriver.v
+++ gatedriver.v
@@ -1,9 +1,25 @@
 `timescale 1ns / 1ps
-
-
-
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Kharagpur Robosoccer Student's Group 
+// Engineer: Yash Agrawal
+// 
+// Create Date: 22:15:44 04/22/2022 
+// Design Name: 
+// Module Name: motordriver 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 1ps
 module gatedriver(pwm,a,b,c,h,d,brake);
-
 
 input brake;
 input [2:0]h;
@@ -22,35 +38,40 @@ wire f=h[1];
 wire g=h[2];
 
 always@(h or pwm) begin
+	if(pwm) begin
+		if(h==7||h==0) begin
+			k=2'b11;
+			l=2'b00;
+			m=2'b01;
+		end
+		else begin
+//			k[0]=(((~d&~f)|(e&f)|(d&~e))|brake);
+//			k[1]=(((~d&e&~f)|(d&~e&f))|brake);
+//			l[0]=(((~d&~g)|(f&g)|(d&~f))|brake);
+//			l[1]=(((~d&f&~g)|(d&~f&g))|brake);
+//			m[0]=(((~d&~e)|(e&g)|(d&~g))|brake);
+//			m[1]=(((~d&~e&g)|(d&e&~g))|brake);
 
-if(pwm) begin
-if(h==7||h==0)
-begin
-k=2'b11;
-l=2'b00;
-m=2'b01;
-end
-else begin
-k[0]=(((~d&~f)|(e&f)|(d&~e))|brake);
-k[1]=(((~d&e&~f)|(d&~e&f))|brake);
-l[0]=(((~d&~g)|(f&g)|(d&~f))|brake);
-l[1]=(((~d&f&~g)|(d&~f&g))|brake);
-m[0]=(((~d&~e)|(e&g)|(d&~g))|brake);
-m[1]=(((~d&~e&g)|(d&e&~g))|brake);
-end
-end
-else begin
-if(brake==1) begin
-k=2'b11;
-l=2'b11;
-m=2'b11;
-end
-else begin
-k=2'b01;
-l=2'b01;
-m=2'b01;
-end
-end
+			k[0] = (((~d&e&(~f))|(d&(~e)&f))|brake);
+			k[1] = ((((d)|(~f)|e)&((~d)|(~e)|f))|brake);
+			l[0] = (((~d&f&(~g))|(d&(~f)&g))|brake);
+			l[1] = ((((d)|(~g)|f)&((~d)|(~f)|g))|brake);
+			m[0] = (((~d&g&(~e))|(d&(~g)&e))|brake);
+			m[1] = ((((d)|(~e)|g)&((~d)|(~g)|e))|brake);
+		end
+	end
+	else begin
+		if(brake==1) begin
+			k=2'b11;
+			l=2'b11;
+			m=2'b11;
+		end
+		else begin
+			k=2'b01;
+			l=2'b01;
+			m=2'b01;
+		end
+	end
 end
 assign a=k;
 assign b=l;
